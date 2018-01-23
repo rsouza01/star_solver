727c383c9e12521e17ec552fb61d7a5e43224ad3
Rodrigo Souza <rsouza01@gmail.com>
2018-01-23 15:41:25 -0200

    Some new stuff

--- README.md
+++ README.md
@@ -12,3 +12,7 @@
 https://stackoverflow.com/questions/29344196/creating-a-config-file
 
 https://martin-thoma.com/configuration-files-in-python
+
+# Unit tests
+
+https://medium.com/meus-pedidos/tutorial-testes-unit%C3%A1rios-e-python-parte-i-bb77182db93f
---
+++ star_solver/config.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+import configparser
+
+
+def generate_default_config():
+
+
+    config = configparser.ConfigParser()
+
+    config['EOS'] = {}
+    config['EOS']['filename'] = 'eos.csv'
+
+
+    with open('solver.ini', 'w') as configfile:
+        config.write(configfile)
+
+
+def main():
+    generate_default_config()
+
+
+
+
+if __name__ == "__main__":
+        main()
+
