256881557836bd3c14d63214747360518b3a5fdc
Rodrigo Souza <rsouza01@gmail.com>
2018-01-23 16:28:36 -0200

    New references and tests

--- README.md
+++ README.md
@@ -16,3 +16,9 @@ https://martin-thoma.com/configuration-files-in-python
 # Unit tests
 
 https://medium.com/meus-pedidos/tutorial-testes-unit%C3%A1rios-e-python-parte-i-bb77182db93f
+
+
+https://stackoverflow.com/questions/1896918/running-unittest-with-typical-test-directory-structure
+
+
+
---
+++ star_solver/tests/test_config.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+
+import sys, os
+
+testdir = os.path.dirname(__file__)
+srcdir = '../star_solver/'
+sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
+
+import unittest
+
+import config as cfg
+
+
+class ConfigFileTests(unittest.TestCase):
+
+    
+    def test_generate_config(self):
+
+        #Arrange
+
+        #Act
+
+        #Assert
+        pass
